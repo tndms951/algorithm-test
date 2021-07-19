bbebef3994815e4acf1ee23c5df630fdd60e97ff
송지수 <tndms951@naver.com>
2021-07-20 00:10:55 +0900

    average algorithm

---
+++ 01.average/index.js
@@ -0,0 +1,16 @@
+//답1
+function solution(arr) {
+  let limit = 0;
+  for (let i = 0; i < arr.length; i++) {
+    limit = limit + arr[i];
+    limit / arr.length;
+  }
+  return limit / arr.length;
+}
+console.log(solution([1, 2, 3, 4]));
+
+//답2
+function solution2(arr) {
+  return arr.reduce((a, b) => a + b, 0) / arr.length;
+}
+solution2([1, 2, 3, 4]);
